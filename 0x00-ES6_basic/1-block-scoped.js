05410f6e8b8c34d9b5509659d70298242cf62e15
Alvin <alvinvaati@gmail.com>
2023-07-24 23:08:35 +0300

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
+  const task = false;
+  const task2 = true;
 
-    if (trueOrFalse) {
-        let task = true;
-        let task2 = false;
-    }
+  if (trueOrFalse) {
+    const task = false;
+    const task2 = true;
 
     return [task, task2];
+  }
+
+  return [task, task2];
 }
