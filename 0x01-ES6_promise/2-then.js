df2f6df13f8729a44b0184a4d28d14b0dd742a82
Alvin <alvinvaati@gmail.com>
2023-07-27 01:10:16 +0300

    Promise resolves, return an object

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+export default async function handleResponseFromAPI(promise) {
+  promise.then(
+    () => {
+      console.log('Got a response from the API');
+      return (
+        {
+          status: 200,
+          body: 'success',
+        }
+      );
+    },
+  ).catch(
+    (err) => err,
+  );
+}
