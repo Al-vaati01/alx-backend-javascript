9ad14c214d655cb63abb731b1b4cc2151e9be12e
Alvin <alvinvaati@gmail.com>
2023-07-27 17:25:07 +0300

    attribute check

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,10 +1,18 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name === 'string' && typeof length === 'number'
-        && typeof students === 'string') {
+    if (typeof name === 'string') {
       this._name = name;
+    } else {
+      throw TypeError('Name must be string');
+    }
+    if (typeof length === 'number') {
       this._length = length;
+    } else {
+      throw TypeError('Length must be a number');
+    } if (typeof students === 'string') {
       this._students = students;
+    } else {
+      throw TypeError('Must be array of strings');
     }
   }
 
