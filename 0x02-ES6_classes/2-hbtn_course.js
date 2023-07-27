25e7629ed0f9514f30380abdf2131654cfe2e6c3
Alvin <alvinvaati@gmail.com>
2023-07-27 19:26:58 +0300

    removed unnecessary values

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,39 +1,42 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-        this._name;
-        this._length;
-        this._students;
-        if (typeof name === 'string') {
-            this._name = name;
-        } else {
-            throw TypeError('Name must be string');
-        }
-        if (typeof length === 'number') {
-            this._length = length;
-        } else {
-            throw new TypeError('Length must be a number');
-        } if (Array.isArray(students) && students.every((s) => typeof s === 'string')) {
-            this._students = students;
-        } else {
-            throw new TypeError('Length must be a number');
-        }
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw TypeError('Name must be string');
     }
-    get name(){
-        return this._name;
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    } if (Array.isArray(students) && students.every((s) => typeof s === 'string')) {
+      this._students = students;
+    } else {
+      throw new TypeError('Length must be a number');
     }
-    set name(n){
-        this._name = n;
-    }
-    get length(){
-        return this._length;
-    }
-    set length(l){
-        this._length = l;
-    }
-    get students(){
-        return this.students;
-    }
-    set students(s){
-        this._students = s;
-    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(n) {
+    this._name = n;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(l) {
+    this._length = l;
+  }
+
+  get students() {
+    return this.students;
+  }
+
+  set students(s) {
+    this._students = s;
+  }
 }
