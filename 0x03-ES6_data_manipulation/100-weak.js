8c97625e15a38469b94e896ae4e26683d876229c
Alvin <alvinvaati@gmail.com>
2023-08-01 22:58:06 +0300

    weakmap

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,5 +1,5 @@
 export const weakMap = new WeakMap();
-export default function queryAPI(endpoint) {
+export function queryAPI(endpoint) {
   let counter = weakMap.get(endpoint) || 0;
   counter += 1;
   if (counter <= 5) {
