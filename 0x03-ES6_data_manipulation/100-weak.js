5341d8b3d68767cda72637db9f444ec98835a78b
Alvin <alvinvaati@gmail.com>
2023-08-01 22:54:34 +0300

    weakmap

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,7 +4,7 @@ export default function queryAPI(endpoint) {
   let counter = weakMap.get(endpoint) || 0;
   counter += 1;
 
-  if (counter <= 5) {
+  if (counter < 5) {
     weakMap.set(endpoint, counter);
   } else {
     throw new Error('Endpoint load is high');
