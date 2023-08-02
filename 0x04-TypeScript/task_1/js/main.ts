c807b7749dd49e09ca57e15f8786ecb23f9fe944
Alvin <alvinvaati@gmail.com>
2023-08-02 15:52:48 +0300

    print taechers name ie J. doe

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,9 @@ interface Teacher{
 interface Directors extends Teacher{
     numberOfReports : number;
 }
+
+function printTeacher(firstName : string, lastName : string) : string{
+    const f_letter = firstName[0];
+    const intialName = f_letter.concat('. ',lastName);
+    return intialName;
+}
