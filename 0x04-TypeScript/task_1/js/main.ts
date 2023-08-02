44f44f72e9ebd957fd0439d513f345ed63a9d5a3
Alvin <alvinvaati@gmail.com>
2023-08-02 17:18:32 +0300

    task1 main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,18 +1,41 @@
-interface Teacher{
-    readonly firstName : string,
-    readonly lastName : string,
-    fullTimeEmployee : boolean,
-    yearsOfExperience ?: number,
-    location : string,
-    [key : string]: any
+interface Teacher {
+    readonly firstName: string,
+    readonly lastName: string,
+    fullTimeEmployee: boolean,
+    yearsOfExperience?: number,
+    location: string,
+    [key: string]: any
 };
 
-interface Directors extends Teacher{
-    numberOfReports : number;
+interface Directors extends Teacher {
+    numberOfReports: number;
 }
 
-function printTeacher(firstName : string, lastName : string) : string{
+function printTeacher(firstName: string, lastName: string): string {
     const f_letter = firstName[0];
-    const intialName = f_letter.concat('. ',lastName);
+    const intialName = f_letter.concat('. ', lastName);
     return intialName;
+}
+interface studentConstructor {
+    firstName: string,
+    lastName: string
+}
+interface studentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+class StudentClass implements studentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor({ firstName, lastName }: studentConstructor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+    displayName(): string {
+        return this.firstName;
+    }
 }
