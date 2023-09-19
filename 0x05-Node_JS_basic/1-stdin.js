f31bb11492489833eee78e3091a3bafce3cc4e4f
Alvin <alvinvaati@gmail.com>
2023-09-19 18:46:25 +0300

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (chunk) => {
+  const name = chunk.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
