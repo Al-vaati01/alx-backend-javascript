d5c5a995b8ae762d995b001e726d1ea0fe7cdc85
Alvin <alvinvaati@gmail.com>
2023-09-20 09:53:00 +0300

    display message

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Alvin
+ */
 const displayMessage = (msg)=>console.log(msg);
 module.exports = displayMessage;
