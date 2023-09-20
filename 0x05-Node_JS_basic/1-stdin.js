9cecbc354aaee2e5ac40d5050b55bd9558adda25
Alvin <alvinvaati@gmail.com>
2023-09-20 10:16:36 +0300

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-console.log('Welcome to Holberton School, what is your name?');
-
-process.stdin.on('data', (chunk) => {
-  const name = chunk.toString().trim();
-  console.log(`Your name is: ${name}`);
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
 });
-
-process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
 });
