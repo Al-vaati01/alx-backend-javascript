3cb226bdae075629c41aed38ca7a6c89144e7acb
Alvin <alvinvaati@gmail.com>
2023-09-22 16:49:04 +0300

    HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,10 +2,16 @@
 const http = require('http');
 
 const PORT = 1245;
+const HOST = 'localhost';
 
-const app = http.createServer((req, res) => {
-  res.write('Hello Holberton School!');
-  res.end();
+const app = http.createServer();
+
+app.on('request', (_, res) => {
+    res.write('Hello Holberton School!');
+    res.statusCode = 200;
+    res.end();
+})
+
+app.listen(PORT, HOST, ()=>{
+    process.stdout.write(`Server listening on Port: ${PORT} in ${HOST}`);
 });
-
-app.listen(PORT);
