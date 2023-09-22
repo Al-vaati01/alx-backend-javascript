fca01b59b4142225189fb1b19f327387da1b6c4e
Alvin <alvinvaati@gmail.com>
2023-09-22 16:55:43 +0300

    HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -7,13 +7,13 @@ const HOST = 'localhost';
 const app = http.createServer();
 
 app.on('request', (_, res) => {
-    const response = 'Hello Holberton School!';
-    res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Content-Length', response.length);
-    res.statusCode = 200;
-    res.write(response);
-})
+  const response = 'Hello Holberton School!';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', response.length);
+  res.statusCode = 200;
+  res.write(response);
+});
 
 app.listen(PORT, HOST, () => {
-    process.stdout.write(`Server listening on Port: ${PORT} in ${HOST}`);
+  process.stdout.write(`Server listening on Port: ${PORT} in ${HOST}`);
 });
