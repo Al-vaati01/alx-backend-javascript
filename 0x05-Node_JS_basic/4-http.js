447f1fa5f370ead3d06ebeb23f26e5b2eabfb800
Alvin <alvinvaati@gmail.com>
2023-09-22 16:35:35 +0300

    HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const http = require('http');
+
+const PORT = 1234;
+
+const app = http.createServer((req, res) => {
+  res.write('Hello Holberton School!');
+  res.end();
+});
+
+app.listen(PORT);
